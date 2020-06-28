d6e43fa0cea98181b4ab9c82769a36b40cb0405a
Shivix <52997365+Shivix@users.noreply.github.com>
2020-06-28 09:31:48 +0100

    Implemented move constructor

--- include/GNN/neuron.hpp
+++ include/GNN/neuron.hpp
@@ -4,13 +4,13 @@
 #include <vector>
 
 namespace GNN{
-    static std::random_device randomDevice;
-    static std::mt19937 engine(randomDevice());
+    static std::random_device randomDevice; // used to seed RNG engine. Will attempt to use hardware entropy if possible
+    static std::mt19937 engine(randomDevice()); // Mersenne-twister engine used for random number generation
     static std::uniform_real_distribution<float> weightDistribution(-1, 1.0); // calling as static gives less overhead
     
     class neuron{
     public:
-        explicit inline neuron(int connections){
+        explicit inline neuron(int connections){ // randomly sets all weights and adds the connections to a vector
             connectionVec.resize(connections);
             int inputIter = -1;
             for(auto&& i: connectionVec){
@@ -18,39 +18,42 @@ namespace GNN{
                 i.input = ++inputIter;
             }
         }
-        neuron(const neuron &neuron) = default;
+        neuron(const neuron& neuron) = default;
+        neuron(neuron&& neuron):
+            value(std::move(neuron.value)),
+            connectionVec(std::move(neuron.connectionVec)),
+            bias(neuron.bias)
+        {};
         ~neuron()= default;
-    
-    
+        
     public:
         float value = 0.0f;
         struct connection{ // connections going IN to the neuron
             float weight;
             int input;
         };
-        inline std::vector<connection> getConnectionVec(){
+        
+        typedef void (*func)(float a);
+        void activation(func activationFunc) const{
+            activationFunc(value);
+        }
+        inline auto getConnectionVec() -> std::vector<connection>& {
             return connectionVec;
+        }
+        void sigmoid() { // runs when a neuron is fed from all previous neurons
+            value = value / (1 + std::abs(value));
+        }
+        void tanh() const { // runs when a neuron is fed from all previous neurons
+            std::tanh(value);
         }
         template <typename charT, typename traits>
         friend std::basic_ostream<charT,traits>& operator<<(std::basic_ostream<charT,traits>& os, const connection& connection){// makes the content of a neuron easier to output.
             os << "Weight:" << connection.weight << "from:" << connection.input << "neuron\n";
             return os;
         }
-        void sigmoid() { // runs when a neuron is fed from all previous neurons
-            value = value / (1 + (std::abs(value)));
-        }
-        void tanh() const { // runs when a neuron is fed from all previous neurons
-            std::tanh(value);
-        }
-
-        typedef void (*func)(float a);
-        void activation(func activationFunc) const{
-            activationFunc(value);
-        }
         
     private:
         float bias = 0;
-        
         std::vector<connection> connectionVec;
     };
 }
